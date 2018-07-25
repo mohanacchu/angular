8519558bb4fb22a6ab59a60c6e224b638cdf584b
mohanacchu <mohangowda1994@gmail.com>
2018-07-25 16:06:20 +0530

    function added

--- javascript.js
+++ javascript.js
@@ -26,22 +26,45 @@ console.log(bound(1,2,3));
 
 // general way
 
-var obj = {
-    num:2
-};
+// var obj = {
+//     num:2
+// };
 
-var functionName = function(arg1,arg2,arg3){
+// var functionName = function(arg1,arg2,arg3){
 
-};
+// };
 
-functionName.call(obj,arg1,arg2,arg3);// call function
-functionName.apply(obj,[arg1,arg2,arg3]);// apply function
+// functionName.call(obj,arg1,arg2,arg3);// call function
+// functionName.apply(obj,[arg1,arg2,arg3]);// apply function
 
-var bound = functionName(obj);
+// var bound = functionName(obj);
 
-bound(arg1,arg2,arg3);
+// bound(arg1,arg2,arg3);
 
 //end
 
 
+// functions curve
 
+function add(first,second){
+    
+    first+second;
+   return "hoos";
+}
+var sum = add(5,5);
+console.log(sum);
+// alert (add(5,5)); 
+
+ 
+function go(name,age){
+    if(age<25){
+        return name + "!";
+    }
+    else{
+        return name;
+    }
+}
+
+var ages = go("mohan",23);
+
+console.log(ages);
